6a7d9cf4fd5a7268148d4a454fbd52e82dd299fa
nsp4746 <nsp4746@rit.edu>
2024-09-21 12:02:58 -0400

    Substitution Cipher

--- CC Receiver.py
+++ CC Receiver.py
@@ -1,18 +1,49 @@
-import os
+# Nikhil Patil
+# CSEC 751
+# Covert Channel Receiver
+
+import os, string
 from pathlib import Path
 
-FILEPATH = os.curdir + "\\files\\"
-os.chdir(FILEPATH)
+class Receive:
+    def __init__(self):
+        self.filepath = os.curdir + "\\files\\"
+        os.chdir(self.filepath)
+        self.message = ""
+        self.all_letters = string.ascii_letters
+        self.decrypted_message = []
+        self.decryption_dictionary = {}
+        key = 4
+        for i in range(len(self.all_letters)):
+            self.decryption_dictionary[self.all_letters[i]] = self.all_letters[(i - key) % (len(self.all_letters))]
+
+    def receive_message(self):
+        paths = sorted(Path().iterdir(), key=os.path.getmtime)
+        for cf in paths:
+            self.message = self.message + str(cf)[5]
+
+    def decrypt_message(self):
+        for char in self.message:
+            if char in self.all_letters:
+                temp = self.decryption_dictionary[char]
+                self.decrypted_message.append(temp)
+            else:
+                temp = char
+                self.decrypted_message.append(temp)
+        self.decrypted_message = "".join(self.decrypted_message)
+
+        for filename in os.listdir(os.curdir):
+            os.remove(os.curdir + "\\" + filename)
 
 
-message = ""
-paths = sorted(Path().iterdir(), key=os.path.getmtime)
+        return self.decrypted_message
 
 
-for cf in paths:
-    message = message + str(cf)[5]
+def main():
+    receiver = Receive()
+    receiver.receive_message()
+    receiver.decrypt_message()
+    print(receiver.decrypted_message)
 
-print("message:" + message)
-
-for filename in os.listdir(os.curdir):
-        os.remove(os.curdir + "\\" + filename)
+if __name__ == "__main__":
+    main()
--- CC Sender.py
+++ CC Sender.py
@@ -1,18 +1,54 @@
 # Nikhil Patil
 # CSEC 751
-# Covert Channel Example
+# Covert Channel Sender
 
-import os, random, time
+import os, random, time, string
 
-FILEPATH = os.curdir + "\\files\\"
 
-message = "i love you!!!!!!!!!!"
+class Sender:
+    def __init__(self, message):
+        self.FILEPATH = os.curdir + "\\files\\"
+        os.chdir(self.FILEPATH)
+        self.message = message
+        self.all_letters = string.ascii_letters
+        self.cipher_message = []
+        self.encryption_dictionary = {}
+        key = 4
+        for i in range(len(self.all_letters)):
+            self.encryption_dictionary[self.all_letters[i]] = self.all_letters[(i + key) % len(self.all_letters)]
 
-os.chdir(FILEPATH)
+    def __str__(self):
+        return f'Message Sent: {self.message}'
 
-for char in message:
-    filename = "file_" + char + "_" + str(random.randint(1,1000))
-    f = open(filename, "w")
-    f.close()
-    time.sleep(2)
+    def set_message(self, message):
+        self.message = message
+
+
+    def create_files(self):
+        for char in self.message:
+            filename = "file_" + char + "_" + str(random.randint(1, 1000))
+            f = open(filename, "w")
+            f.close()
+            time.sleep(.5)
+
+    def encrypt_message(self):
+        for char in self.message:
+            if char in self.all_letters:
+                temp =  self.encryption_dictionary[char]
+                self.cipher_message.append(temp)
+            else:
+                temp = char
+                self.cipher_message.append(temp)
+
+        self.cipher_message = "".join(self.cipher_message)
+        self.message = self.cipher_message
+
+
+def main():
+    sender = Sender("i am studying covert channels at rit")
+    sender.encrypt_message()
+    sender.create_files()
+
+if __name__ == "__main__":
+    main()
 
